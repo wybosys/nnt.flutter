e615494281a59f17ea7d94cc053b14a603485625
wybosys <wybosys@gmail.com>
2019-07-24 15:07:35 +0800

    prj

--- nnt/lib/main.dart
+++ nnt/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page X'),
     );
   }
 }
--- nnt/test/widget_test.dart
+++ nnt/test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:nnt/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke testx', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
