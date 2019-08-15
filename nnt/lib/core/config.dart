6d9afd5d21bf5b7a91f88d3a04b3c7357ee52ad4
wybosys <wybosys@gmail.com>
2019-08-15 20:19:07 +0800

    prj

---
+++ nnt/lib/core/config.dart
@@ -0,0 +1,31 @@
+library nnt.core;
+
+class _Config {
+  void override(Map obj) {
+    obj.forEach((k, v) {
+      _cur[k] = v;
+    });
+  }
+
+  dynamic get(String key, [dynamic def = null]) {
+    return _cur.containsKey(key) ? this._cur[key] : def;
+  }
+
+  void set(String key, dynamic val) {
+    _cur[key] = val;
+  }
+
+  void delete(String key) {
+    if (_cur.containsKey(key)) {
+      _cur.remove(key);
+    }
+  }
+
+  bool contains(String key) {
+    return _cur.containsKey(key);
+  }
+
+  Map<String, dynamic> _cur = new Map();
+}
+
+final config = new _Config();
