f68eed1ab3a236cdd31b5099ad77f74e1a76da19
wybosys <wybosys@gmail.com>
2019-08-16 21:12:51 +0800

    anno

--- nnt/lib/annotation.dart
+++ nnt/lib/annotation.dart
@@ -1,4 +1,8 @@
 library nnt.annotation;
 
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
+
 part 'annotation/clazz.dart';
 part 'annotation/~gen.dart';
--- nnt/lib/annotation/clazz.dart
+++ nnt/lib/annotation/clazz.dart
@@ -1,3 +1,13 @@
 part of nnt.annotation;
 
-class Clazz {}
+class Clazzes {}
+
+Builder ClazzBuilder<T>(BuilderOptions options) {
+  return SharedPartBuilder([ClazzGenerator<T>()], 'clazz_generator');
+}
+
+class ClazzGenerator<T> extends GeneratorForAnnotation<T> {
+  @override
+  generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {}
+}
