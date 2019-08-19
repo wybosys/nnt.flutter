2424851e5fff2f7099b71f943ddd1e09f3f8156d
wybosys <wybosys@gmail.com>
2019-08-19 17:31:10 +0800

    prj

--- sample/lib/app.dart
+++ sample/lib/app.dart
@@ -3,6 +3,7 @@ library app;
 import 'package:flutter/material.dart';
 import 'package:nnt/annotation.dart';
 import 'package:impl/gui.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 part 'app/home.dart';
 part 'app.g.dart';
