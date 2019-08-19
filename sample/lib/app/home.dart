195623d10e0b05334bf15d76cfd13ef143c3ca02
wybosys <wybosys@gmail.com>
2019-08-19 16:02:48 +0800

    prj

--- sample/lib/app/home.dart
+++ sample/lib/app/home.dart
@@ -2,7 +2,7 @@ part of app;
 
 @clazz()
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage() : super();
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -12,8 +12,6 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -45,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('HELLO'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
