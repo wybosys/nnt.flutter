e2e0a9da9327fd0853387874724b4c7ede927320
wybosys <wybosys@gmail.com>
2019-08-26 19:31:01 +0800

    light

--- impl/android/src/main/kotlin/com/nnt/gui/Activity.kt
+++ impl/android/src/main/kotlin/com/nnt/gui/Activity.kt
@@ -2,6 +2,7 @@ package com.nnt.gui;
 
 import android.content.Context
 import android.os.PowerManager
+import android.view.WindowManager
 import io.flutter.app.FlutterActivity
 
 open class Activity : FlutterActivity() {
@@ -12,6 +13,8 @@ open class Activity : FlutterActivity() {
         }
     var _wakelck: PowerManager.WakeLock? = null
 
+    var _shouldAlwaysLight: Boolean = false
+
     // 保持常亮
     fun alwaysLight(turnon: Boolean) {
         if (turnon == (_wakelck != null)) {
@@ -19,25 +22,33 @@ open class Activity : FlutterActivity() {
             return
         }
 
+        _shouldAlwaysLight = turnon
+
         if (turnon) {
             _wakelck = _powermgr.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "::nnt::activity::light")
-            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         } else {
-            //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
             if (_wakelck?.isHeld() == true) {
                 _wakelck?.release()
                 _wakelck = null
             }
+
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         }
     }
 
     override fun onResume() {
         super.onResume()
         _wakelck?.acquire(30000);
+        if (_shouldAlwaysLight) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
     }
 
     override fun onPause() {
         super.onPause()
         _wakelck?.release();
+        if (_shouldAlwaysLight) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
     }
 }
