a83202fad4f8f21eae7e175a13cfa70a359a2471
wybosys <wybosys@gmail.com>
2019-09-02 16:39:29 +0800

    clean

--- nnt/lib/gui/jsb/src/sample/main.ts
+++
@@ -1,9 +0,0 @@
-// 映射dart类到js中的模板
-export class Test extends nnt.flutter.JsObject {
-    hello() {
-        return nnt.flutter.jsb.toApp(new nnt.flutter.Message(this.objectId, 'hello'));
-    }
-}
-
-var test = new Test();
-test.objectId = 123;
