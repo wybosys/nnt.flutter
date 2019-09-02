4109f382abfc14cf3d0c0da895cae655425931e4
wybosys <wybosys@gmail.com>
2019-09-02 14:46:48 +0800

    udid

---
+++ impl/android/src/main/kotlin/com/nnt/core/Udid.kt
@@ -0,0 +1,63 @@
+package com.nnt.core;
+
+import android.content.Context
+import android.os.Build
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import java.util.*
+
+class Udid {
+
+    companion object {
+
+        @JvmStatic
+        fun GetCurrent(context: Context): String {
+            try {
+                return GetDeviceId(context)
+            } catch (ex: Exception) {
+                Logger.log(ex.toString());
+            }
+            try {
+                return GetPseudoId(context);
+            } catch (ex: Exception) {
+                Logger.log(ex.toString());
+            }
+            try {
+                return GetAndroidId(context)
+            } catch (ex: Exception) {
+                Logger.log(ex.toString());
+            }
+            return ToString(UUID.randomUUID());
+        }
+
+        @JvmStatic
+        fun GetAndroidId(context: Context): String {
+            return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID);
+        }
+
+        @JvmStatic
+        fun GetDeviceId(context: Context): String {
+            val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            val tmDevice = tm.deviceId
+            val tmSerial = tm.simSerialNumber
+            val androidId = android.provider.Settings.Secure.getString(context.contentResolver, android.provider.Settings.Secure.ANDROID_ID)
+            return ToString(UUID(androidId.hashCode().toLong(), tmDevice.hashCode().toLong() shl 32 or tmSerial.hashCode().toLong()))
+        }
+
+        @JvmStatic
+        fun GetPseudoId(context: Context): String {
+            val m_szDevIDShort = "NNT_" + (Build.BOARD.length % 10) +
+                    (Build.BRAND.length % 10) + (Build.CPU_ABI.length % 10) +
+                    (Build.DEVICE.length % 10) + (Build.MANUFACTURER.length %
+                    10) + (Build.MODEL.length % 10) + (Build.PRODUCT.length %
+                    10)
+            var serial = android.os.Build.getSerial()
+            return ToString(UUID(m_szDevIDShort.hashCode().toLong(), serial.hashCode().toLong()))
+        }
+
+        @JvmStatic
+        fun ToString(id: UUID): String {
+            return id.toString().replace("-", "");
+        }
+    }
+}
--- impl/android/src/main/kotlin/com/nnt/impl/impl/ImplPlugin.kt
+++ impl/android/src/main/kotlin/com/nnt/impl/impl/ImplPlugin.kt
@@ -1,5 +1,6 @@
 package com.nnt.impl.impl
 
+import com.nnt.core.Udid
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -9,8 +10,17 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 class ImplPlugin : MethodCallHandler {
 
     companion object {
+        var _Registrar: Registrar? = null
+
+        val Registrar: Registrar
+            get() {
+                return _Registrar as Registrar
+            }
+
         @JvmStatic
         fun registerWith(registrar: Registrar) {
+            _Registrar = registrar;
+
             val channel = MethodChannel(registrar.messenger(), "impl")
             channel.setMethodCallHandler(ImplPlugin())
         }
@@ -22,7 +32,7 @@ class ImplPlugin : MethodCallHandler {
                 result.success("Android ${android.os.Build.VERSION.RELEASE}")
             }
             "getUdid" -> {
-
+                result.success(Udid.GetCurrent(Registrar.context()));
             }
             else -> {
                 result.notImplemented()
