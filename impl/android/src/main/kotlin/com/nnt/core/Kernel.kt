caec316e5470794684819ec1c6d233ea250c86f9
wybosys <wybosys@gmail.com>
2019-09-04 11:15:56 +0800

    prj

--- impl/android/src/main/kotlin/com/nnt/core/Kernel.kt
+++ impl/android/src/main/kotlin/com/nnt/core/Kernel.kt
@@ -5,3 +5,75 @@ typealias IndexedObject = AnyMap
 typealias KeyPath = String
 
 val Nil: Any = {}
+
+fun toInt(v: Any?, def: Int = 0): Int {
+    if (v == null) {
+        return def
+    }
+    if (v is Int) {
+        return v
+    }
+    if (v is Double) {
+        return v.toInt()
+    }
+    if (v is Float) {
+        return v.toInt()
+    }
+    if (v is Number) {
+        return v.toInt()
+    }
+    if (v is Boolean) {
+        return if (v) 1 else 0
+    }
+    if (v is String) {
+        return v.toIntOrNull() ?: def
+    }
+    return def
+}
+
+fun toDouble(v: Any?, def: Double = 0.0): Double {
+    if (v == null) {
+        return def
+    }
+    if (v is Int) {
+        return v.toDouble()
+    }
+    if (v is Double) {
+        return v.toDouble()
+    }
+    if (v is Float) {
+        return v.toDouble()
+    }
+    if (v is Number) {
+        return v.toDouble()
+    }
+    if (v is Boolean) {
+        return if (v) 1.0 else 0.0
+    }
+    if (v is String) {
+        return v.toDoubleOrNull() ?: def
+    }
+    return def
+}
+
+fun toString(v:Any?, def: String = ""):String {
+    if (v == null) {
+        return def
+    }
+    if (v is String) {
+        return v
+    }
+    if (v is Int) {
+        return v.toString()
+    }
+    if (v is Double) {
+        return v.toString()
+    }
+    if (v is Float) {
+        return v.toString()
+    }
+    if (v is Number) {
+        return v.toString()
+    }
+    return v.toString()
+}
